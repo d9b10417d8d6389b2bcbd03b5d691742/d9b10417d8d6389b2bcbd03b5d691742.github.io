3d9ae7f15bd63b8ba920836cbbdabea07b3607f1
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-11-01 13:27:44 +0100

    u

---
+++ js/cardloader.js
@@ -0,0 +1,22 @@
+$(function() {
+    //$('.cardwrapper').toggle();
+    $.ajax({
+            type: "GET",
+            url: "xml/main.xml",
+            success: mainLoader
+    });
+});
+
+function mainLoader (basexml) {
+    $(basexml).find("xmlname").each(function() {
+        $.ajax({
+            type: "GET",
+            url: "xml/" + $(this).text() + ".xml",
+            success: function(xml) {
+            $(".cardwrapper").append('<div class="card" onclick="OpenPage(\'creation?=' + $(xml).find("filename").text() + '\')"><div class="cardimgbox" version="' + $(xml).find("version").text() + '"><img src="https://img.youtube.com/vi/' + $(xml).find("videourl").text() + '/hqdefault.jpg"></div><h2>' + $(xml).find("name").text() + '</h2></div>');
+}
+
+        });
+    });
+};    
+

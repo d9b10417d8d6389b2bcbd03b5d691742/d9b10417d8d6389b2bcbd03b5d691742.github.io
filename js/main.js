2683fe364fcf97341886788782ec48ffff2a80eb
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-11-11 23:11:33 +0100

    u

--- js/main.js
+++ js/main.js
@@ -13,5 +13,4 @@ $(window).on("scroll", function() {
     } else {
         $("nav").removeClass("darknavbar");
     };
-//    $("body").css("background-position", "center " +(scTop * 0.8)+ "px");
-    });
+});

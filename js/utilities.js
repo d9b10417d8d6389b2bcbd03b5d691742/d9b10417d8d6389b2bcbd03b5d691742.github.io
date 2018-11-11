1925617281f4aef03f92038f47f217ac12113f3f
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-11-11 19:19:20 +0100

    u

--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,9 @@ function CopyCommand() {
     $(".commandbox").focus().select();
     document.execCommand("copy");
     $(".commandbox");
+    setTimeout(function(){
+        Msg("The command has been copied to your clipboard.", "copy")
+    }, 100)
 };
 
 function GetCommand() {
@@ -18,4 +21,33 @@ function GetHelp() {
 
 function OpenPage(page) {
     window.location = '/'+ page;
+};
+
+var msgbox = false;
+function Msg(msg, iconname) {
+    if (!msgbox) {
+        var icon = "";
+        if(iconname == "copy") {
+            icon = "<i class='far fa-copy' style='color: gray'></i>";
+        };
+        if(iconname == "no") {
+            icon = "<i class='far fa-times-circle' style='color: darkred'></i>";
+        };
+        if(iconname == "yes") {
+            icon = "<i class='fas fa-check' style='color: limegreen'></i>";
+        };
+        $("content").prepend("<div class='msgbox'>" + icon + "<p>" + msg + "</p><div><button onclick='CloseMsgBox()'>Ok</button></div></div>");
+        $(".msgbox").fadeIn(200);
+        msgbox = true;
+    }
+};
+
+function CloseMsgBox() {
+    $(".msgbox").fadeOut(200);
+    $(".msgbox").addClass("deletedsmgbox");
+    $(".msgbox").removeClass("msgbox");
+    setTimeout(function() {
+        $('.deletedsmgbox').remove();
+        msgbox = false;
+    }, 200);
 }

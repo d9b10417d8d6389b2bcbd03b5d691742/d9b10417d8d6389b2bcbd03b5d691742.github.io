17a0f7694636e879a5a5474630bdea574a71349b
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-11-25 01:10:03 +0100

    u

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ $(function() {
     });
     var scTop = $(window).scrollTop();
     $("body").css("background-position", "center " +(scTop * 0.8)+ "px");
-    });
+    var date = new Date();
+    var month = date.getMonth();
+    if (month === 10) {
+        $("body").prepend("<canvas id=\"backcanvas\"></canvas><script src=\"js/snow.js\"></script>");
+    }   
+});
             
 $(window).on("scroll", function() {
     var scTop = $(window).scrollTop();

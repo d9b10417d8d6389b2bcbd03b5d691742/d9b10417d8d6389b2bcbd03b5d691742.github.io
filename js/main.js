64575c01b9d9fc3de0790c6d756182b388018579
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-12-15 02:05:03 +0100

    u

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
+/*Script of commandjam.tk
+Copyright © 2018 CommandJam - All Rights Reserved*/
 $(function() { $(".menuicon").children().on("click", function() { $(".menu").toggleClass("menuopen"); }); var scTop = $(window).scrollTop(); $("body").css("background-position", "center " +(scTop * 0.8)+ "px"); var date = new Date(); var month = date.getMonth(); if (month === 11) { $("body").prepend("<canvas id=\"backcanvas\"></canvas><script src=\"js/snow.js\"></script>"); } }); $(window).on("scroll", function() { var scTop = $(window).scrollTop(); if(scTop > 100) { $("nav").addClass("darknavbar"); } else { $("nav").removeClass("darknavbar"); };});

4739b1d789277fd4077f4bbbe5a9f2658e77d54f
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2018-12-17 21:42:24 +0100

    u

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 /*Script of commandjam.tk
 Copyright © 2018 CommandJam - All Rights Reserved*/
-$(function () { $(".menuicon").children().on("click", function () { $(".menu").toggleClass("menuopen"); }); var scTop = $(window).scrollTop(); holidaytheme(); cookieconsent();});$(window).on("scroll", function () { var scTop = $(window).scrollTop(); if (scTop > 100) { $("nav").addClass("darknavbar"); } else { $("nav").removeClass("darknavbar"); };});function cookieconsent() { window.cookieconsent.initialise({ "palette": { "popup": { "background": "#111215", "text": "#a09da2" }, "button": { "background": "#306bac" } }, "content": { "href": "https://www.commandjam.tk/privacypolicy" } })}function holidaytheme() { var date = new Date(); var month = date.getMonth(); if (month === 11) { $("body").prepend("<canvas id=\"backcanvas\"></canvas><script src=\"js/snow.js\"></script>"); }}
+$(function () {
+    $(".menuicon").children().on("click", function () {
+        $(".menu").toggleClass("menuopen");
+    });
+    var scTop = $(window).scrollTop();
+    holidaytheme();
+    cookieconsent();
+});
+$(window).on("scroll", function () {
+    var scTop = $(window).scrollTop();
+    if (scTop > 100) {
+        $("nav").addClass("darknavbar");
+    } else {
+        $("nav").removeClass("darknavbar");
+    };
+});
+
+function cookieconsent() {
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#111215",
+                "text": "#a09da2"
+            },
+            "button": {
+                "background": "#306bac"
+            }
+        },
+        "content": {
+            "href": "https://www.commandjam.tk/privacypolicy"
+        }
+    })
+}
+
+function holidaytheme() {
+    var date = new Date();
+    var month = date.getMonth();
+    if (month === 11) {
+        $("body").prepend("<canvas id=\"backcanvas\"></canvas><script src=\"js/snow.js\"></script>");
+    }
+}
